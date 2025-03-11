56a756528ad80725871b599f8aa8fe933cf004ac
Nito <100199931+Nitoryu008@users.noreply.github.com>
2025-03-11 14:00:50 +0900

    chore: add lint

---
+++ stylelint.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  plugins: ["stylelint-declaration-block-no-ignored-properties"],
+  rules: {
+    "plugin/declaration-block-no-ignored-properties": true,
+  },
+};
